5f3a0f10160451cc7df5bb2e81b8b5e8d7f7805f
hws522 <sraphic@kakao.com>
2022-06-22 23:14:50 +0900

    move animation

--- src/gps/MoveAnimation.js
+++ src/gps/MoveAnimation.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import 'ol/ol.css';  //* 스타일
 import Feature from 'ol/Feature';
-import { Map, View, Control } from 'ol';  //* 뷰 관리
+import { Map, View } from 'ol';  //* 뷰 관리
 import Point from 'ol/geom/Point'; //* mark 지정
 import { Icon, Style, Circle, Fill, Stroke } from 'ol/style'; //* mark style 모양
 import { XYZ, Vector as VectorSource } from 'ol/source';  //* 지도정보
@@ -9,64 +9,63 @@ import { fromLonLat, get as getProjection } from 'ol/proj'; //* 위경도
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer'; //* 지도타일
 import { getVectorContext } from 'ol/render'; //* event.vectorContext
 import LineString from 'ol/geom/LineString.js'; //* 좌표로 지도에 경로 그리기
-
 import UseInterval from '../customHooks/UseInterval';
 import markPoint from './arrow.svg'
 import { coordData as polyline } from '../coordData/CoordData';
 
-/**
- * I think the feature-move-animation example was misleading for you. 
- * There is no need to use ol.format.PolyLine to programmatically create a polyline. 
- * Instead, you just create a feature with a polyline geometry and add that to your vector source:
- */
-
 function MoveAnimation() {
     const CENTER = [129.3097937, 35.5428678];
-    let distance = 0;
 
-    const [lastTime, setLastTime] = useState();
+    const [center, setCenter] = useState(null);
+    const [count, setCount] = useState(0);
+    const [now, setNow] = useState(0);
     const [animate, setAnimate] = useState(false);
     const [mapObject, setMapObject] = useState(null);
     const [tileLayer, setTileLayer] = useState(null);
     const [source, setSource] = useState(null);
     const [vectorLayer, setVectorLayer] = useState(null);
 
+    const route = new LineString(polyline).transform('EPSG:4326', 'EPSG:3857');
+    const routeCoords = route.getCoordinates();
+    const routeLength = routeCoords.length;
 
     const routeFeature = new Feature({
         type: 'route',
-        geometry: new LineString(polyline).transform('EPSG:4326', 'EPSG:3857'),
+        // geometry: new LineString(polyline).transform('EPSG:4326', 'EPSG:3857'),
+        geometry: route,
+    });
+    const geoMarker = new Feature({
+        type: 'geoMarker',
+        // geometry: new Point(polyline[0]).transform('EPSG:4326', 'EPSG:3857')
+        geometry: new Point(routeCoords[0])
     });
     const startMarker = new Feature({
         type: 'icon',
-        geometry: new Point(polyline[0]).transform('EPSG:4326', 'EPSG:3857')
+        // geometry: new Point(polyline[0]).transform('EPSG:4326', 'EPSG:3857')
+        geometry: new Point(routeCoords[0])
     });
     const endMarker = new Feature({
         type: 'icon',
-        geometry: new Point(polyline[polyline.length - 1]).transform('EPSG:4326', 'EPSG:3857')
-    });
-    const position = startMarker.getGeometry().clone();
-    const geoMarker = new Feature({
-        type: 'geoMarker',
-        geometry: position,
+        // geometry: new Point(polyline[polyline.length - 1]).transform('EPSG:4326', 'EPSG:3857')
+        geometry: new Point(routeCoords[routeLength - 1])
     });
 
     const styles = {
         'route': new Style({
             stroke: new Stroke({
                 width: 6,
-                color: [237, 212, 0, 1]
+                color: [137, 112, 200, 0.7]
             })
         }),
         'icon': new Style({
             image: new Circle({
-                radius: 7,
+                radius: 5,
                 snapToPixel: false,
                 fill: new Fill({ color: 'black' }),
                 stroke: new Stroke({
                     color: 'white', width: 2
                 })
             }),
-
         }),
         'geoMarker': new Style({
             image: new Icon({
@@ -77,8 +76,6 @@ function MoveAnimation() {
 
     const createMap = () => {
         let tileLayer = new TileLayer({
-            name: 'Base',
-            visible: true,
             source: new XYZ({
                 url: `http://mt0.google.com/vt/lyrs=m&hl=kr&x={x}&y={y}&z={z}&s=Ga`
             })
@@ -92,20 +89,21 @@ function MoveAnimation() {
                 if (animate && feature.get('type') === 'geoMarker') {
                     return null;
                 }
-                return styles[feature.get('type')];
+                else return styles[feature.get('type')];
             }
         });
         let map = new Map({
             layers: [tileLayer, vectorLayer],
-            target: 'map',
+            target: document.getElementById('map'),
+            loadTilesWhileAnimating: true,
             view: new View({
-                projection: getProjection('EPSG:3857'),
-                center: fromLonLat(CENTER, getProjection('EPSG:3857')),
-                zoom: 14,
-                multiWorld: false,
+                // projection: getProjection('EPSG:3857'), 기본값 3857
+                center: fromLonLat(CENTER),
+                zoom: 13,
             }),
         });
 
+        setCenter(map.getView().getCenter());
         setTileLayer(tileLayer);
         setSource(source);
         setVectorLayer(vectorLayer);
@@ -114,64 +112,93 @@ function MoveAnimation() {
 
     function moveFeature(event) {
         if (mapObject) {
-            const speed = 2;
-            const time = event.frameState.time;
-            const elapsedTime = time - lastTime;
-            distance = (distance + (speed * elapsedTime) / 1000000.0) % 2;
-            setLastTime(time);
+            let vectorContext = getVectorContext(event);
+            let frameState = event.frameState;
 
-            const currentCoordinate = new LineString(polyline).transform('EPSG:4326', 'EPSG:3857').getCoordinateAt(
-                distance > 1 ? 2 - distance : distance
-            );
-            position.setCoordinates(currentCoordinate);
-            const vectorContext = getVectorContext(event);
-            vectorContext.setStyle(styles.geoMarker);
-            vectorContext.drawGeometry(position);
+            if (animate && now) {
+                let elapsedTime = frameState.time - now;
+                // here the trick to increase speed is to jump some indexes
+                // on lineString coordinates
+                let index = Math.round(12 * elapsedTime / 1000);
 
-            console.log(currentCoordinate)
-
+                if (index >= routeLength) {
+                    stopAnimation(true);
+                    return;
+                }
+                else {
+                    let feature = new Feature({
+                        type: 'geoMarker',
+                        geometry: new Point(routeCoords[index])
+                    });
+                    vectorContext.drawFeature(feature, styles.geoMarker);
+                }
+            }
+            // tell OL3 to continue the postcompose animation
             mapObject.render();
         }
     };
 
-    function startAnimation(vectorLayer) {
+    function startAnimation() {
+        // console.log('start');
         if (vectorLayer) {
-            setLastTime(Date.now());
-            vectorLayer.on('postrender', moveFeature);
-            geoMarker.setGeometry(null);
+            if (!animate) {
+                console.log('animate')
+                stopAnimation(false);
+            } else {
+                // setAnimate(true);
+                setNow(Date.now());
+                // hide geoMarker
+                geoMarker.setStyle(null);
+                // just in case you pan somewhere else
+                mapObject.getView().setCenter(center);
+                vectorLayer.on('postrender', moveFeature);
+                mapObject.render();
+            }
         }
     }
 
-    function stopAnimation(vectorLayer) {
+    function stopAnimation(ended) {
         if (vectorLayer) {
-            geoMarker.setGeometry(position);
+            // setAnimate(false);
+            // if animation cancelled set the marker at the beginning
+            let coord = ended ? polyline[polyline.length - 1] : polyline[0];
+            geoMarker.getGeometry().setCoordinates(coord);
+            //remove listener
             vectorLayer.un('postrender', moveFeature);
         }
     }
 
+    UseInterval(() => {
+        if (animate) {
+            if (count >= polyline.length) {
+                setCount(0);
+            };
+            if (now) {
+                setNow(Date.now())
+                setCount(count + 1);
+            }
+        }
+    }, 4500)
+
+    useEffect(() => {
+        if (animate) startAnimation()
+        else stopAnimation()
+    }, [now, animate])
+
     //* 처음 렌더링 될 때, 맵 그리기.
     useEffect(() => {
         createMap();
+        //* 시작하자마자 움직이도록.
+        setAnimate(true);
     }, []);
-
-    useEffect(() => {
-        if (!animate) {
-            stopAnimation(vectorLayer);
-        } else {
-            startAnimation(vectorLayer);
-        }
-    }, [animate, vectorLayer])
 
     return (
         <>
             <div id="map"
-                className='map'
-                style={{ height: '50rem' }}>
+                value={mapObject}
+                style={{ height: 500 }}>
             </div>
-            <button id='start-animation'
-                onClick={() => setAnimate(!animate)}>
-                {animate ? 'stop' : 'start'}
-            </button>
+            <button id="start-animation" onClick={() => setAnimate(!animate)}>{animate ? `Stop Animation` : `Start Animation`}</button>
         </>
     )
 }
